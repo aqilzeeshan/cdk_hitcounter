e4c074ad47570a2fb83857ef80aa749ece4efce9
aqilzeeshan <aqilzeeshan@users.noreply.github.com>
2021-02-10 15:44:31 +0000

    Add files

--- lib/cdk_hitcounter-stack.ts
+++ lib/cdk_hitcounter-stack.ts
@@ -1,9 +1,46 @@
 import * as cdk from '@aws-cdk/core';
+import iam = require('@aws-cdk/aws-iam');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
+import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkHitcounterStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+	// =====================================================================================
+    // Hello Lambda for API Gateway
+    // =====================================================================================
+    // Use API Gatetway AWS Proxy template to test it from AWS Console
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "index", function is "handler"
+    });
+
+    // =====================================================================================
+    // API Gateway to call Hello Lambda
+    // =====================================================================================
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    //new apigw.LambdaRestApi(this, 'Endpoint', {
+    //  handler: hello
+    //});
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits'       // optional ("-" denotes descending order)
+    });    
   }
 }
